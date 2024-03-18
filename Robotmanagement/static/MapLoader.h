720e1f37e151be802fdf3423ba509fa9c7b0ff57
Tyler Truong <tctruong912@outlook.com>
2024-03-18 15:24:01 -0700

    Test Program

--- Robotmanagement/static/MapLoader.h
+++ Robotmanagement/static/MapLoader.h
@@ -17,8 +17,4 @@ public:
     static void LoadMap(TileGraph &graph, std::vector<Goal *> &goals, std::vector<Wall *> &walls, std::string fileName);
 
 private:
-    int extractData(std::string fileName);
-    Rect extractBounds(std::string fileName); // Returns [width, height, topLeftXCoord, topLeftYCoord] as GridInfo
-    std::vector<Goal *> extractGoals(std::string fileName);
-    int extractLines(std::string fileName);
 };
