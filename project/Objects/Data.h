f7bd88dea1851268de815406f2cb000ba49d4a1d
Tyler Truong <tctruong912@outlook.com>
2024-04-09 15:41:33 -0700

    Resturcture class design

---
+++ project/Objects/Data.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// Represents the Obstacle points from the map file under the Data section
+#include "MapObject.h"
+#include "../TileGraph/Tile.h"
+#include <iostream>
+
+class Data : public MapObject
+{
+public:
+    Data();
+    Data(int x, int y);
+
+    void setPosition(int x, int y);
+    Type getType() const override { return Type::Wall; }
+
+    void print() override
+    {
+        std::cout << "Data Position: (" << _currentTile->getPosition()._x << ", " << _currentTile->getPosition()._y << ")" << std::endl;
+    }
+
+private:
+};
