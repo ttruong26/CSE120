f758548e9cdec1f6f6975487735f92c86ba028a6
Vansh Venugopal <54867512+6VenugopalV@users.noreply.github.com>
2024-04-22 17:13:58 -0700

    Update pathTimeEstimate.cpp
    includes turning time of 0.5 seconds per 90 degrees.

--- pathTimeEstimate.cpp
+++ pathTimeEstimate.cpp
@@ -10,10 +10,11 @@ using namespace std;
 struct Node {
     int x, y;
     double f, g, h;
+    int directionChange; // Number of direction changes from start to this node
     Node* parent;
 
-    Node(int _x, int _y, double _f, double _g, double _h, Node* _parent = nullptr)
-        : x(_x), y(_y), f(_f), g(_g), h(_h), parent(_parent) {}
+    Node(int _x, int _y, double _f, double _g, double _h, int _directionChange = 0, Node* _parent = nullptr)
+        : x(_x), y(_y), f(_f), g(_g), h(_h), directionChange(_directionChange), parent(_parent) {}
 
     bool operator>(const Node& other) const {
         return f > other.f;
@@ -61,8 +62,7 @@ bool isInObstacle(int x, int y, const vector<pair<int, int>>& obstacles, const v
     return false;
 }
 
-
-// Modify A* search to incorporate collision checking with line segments
+// Modify A* search to incorporate collision checking with line segments and turning time
 double performAStar(int startX, int startY, int goalX, int goalY, double averageSpeed,
                     const vector<pair<int, int>>& obstacles, const vector<pair<int, int>>& lineSegments,
                     int gridSizeX, int gridSizeY) {
@@ -80,8 +80,7 @@ double performAStar(int startX, int startY, int goalX, int goalY, double average
 
         // Check if goal reached
         if (current.x == goalX && current.y == goalY) {
-            //cout << "Distance to goal: " << current.g/1000 << " meters" << endl;
-            return current.g / averageSpeed; // Return time to reach goal
+            return (current.g + current.directionChange * 0.5) / averageSpeed; // Return time to reach goal including turning time
         }
 
         // Check if node is already visited or in an obstacle
@@ -109,6 +108,17 @@ double performAStar(int startX, int startY, int goalX, int goalY, double average
                 // Calculate new cost to reach neighbor node
                 double newCost = current.g + calculateDistance(current.x, current.y, nextX, nextY);
 
+                // Calculate additional time for turning
+                int turningTime = 0;
+                if ((dx != 0 && dy == 0) || (dx == 0 && dy != 0)) {
+                    if ((current.parent != nullptr) && (dx != current.x - current.parent->x || dy != current.y - current.parent->y)) {
+                        turningTime = 0.5;
+                    }
+                }
+
+                // Add turning time to the new costa
+                newCost += turningTime;
+
                 // Check if neighbor node is not visited and not in an obstacle
                 if (!visited[nextX][nextY] && !isInObstacle(nextX, nextY, obstacles, lineSegments)) {
                     // Calculate heuristic cost (distance to goal)
@@ -116,7 +126,7 @@ double performAStar(int startX, int startY, int goalX, int goalY, double average
                     double f = newCost + heuristic; // Total estimated cost
 
                     // Push neighbor node onto open set
-                    openSet.push(Node(nextX, nextY, f, newCost, heuristic));
+                    openSet.push(Node(nextX, nextY, f, newCost, heuristic, current.directionChange + turningTime, &current));
                 }
             }
         }
@@ -125,115 +135,54 @@ double performAStar(int startX, int startY, int goalX, int goalY, double average
     return -1; // Path not found
 }
 
-
 int main() {
-    int minPosX = -29200;
-    int minPosY = -19930;
-    int maxPosX = -5940;
-    int maxPosY = -4280;
+    int minPosX = -45000;
+    int minPosY = -18000;
+    int maxPosX = -10000;
+    int maxPosY = -5000;
     double averageSpeed = 1200; // in mm/sec
 
     // Define obstacle areas
-    vector<pair<int, int> > obstacles;
-     obstacles.push_back(make_pair(-26100, -18640));
-     obstacles.push_back(make_pair(-26100, -18540));
-     obstacles.push_back(make_pair(-26100, -18520));
-     obstacles.push_back(make_pair(-26100, -18440));
-     obstacles.push_back(make_pair(-26100, -18400));
-     obstacles.push_back(make_pair(-26100, -18360));
-     obstacles.push_back(make_pair(-26100, -18340));
-     obstacles.push_back(make_pair(-26100, -18320));
-     obstacles.push_back(make_pair(-26100, -18300));
-     obstacles.push_back(make_pair(-26100, -18280));
-     obstacles.push_back(make_pair(-26100, -18140));
-     obstacles.push_back(make_pair(-26100, -18120));
-     obstacles.push_back(make_pair(-26100, -15700));
-     obstacles.push_back(make_pair(-26080, -18580));
-     obstacles.push_back(make_pair(-26080, -18400));
-     obstacles.push_back(make_pair(-26080, -18160));
-     obstacles.push_back(make_pair(-26080, -18140));
-     obstacles.push_back(make_pair(-26080, -15680));
-     obstacles.push_back(make_pair(-26080, -11960));
-     obstacles.push_back(make_pair(-26060, -18560));
-     obstacles.push_back(make_pair(-26060, -18140));
-     obstacles.push_back(make_pair(-26060, -15680));
-     obstacles.push_back(make_pair(-26040, -18680));
-     obstacles.push_back(make_pair(-26040, -18560));
-     obstacles.push_back(make_pair(-26040, -18200));
-     obstacles.push_back(make_pair(-26040, -18160));
-     obstacles.push_back(make_pair(-26040, -18140));
-     obstacles.push_back(make_pair(-26020, -18600));
-     obstacles.push_back(make_pair(-26020, -18580));
-     obstacles.push_back(make_pair(-26020, -18160));
-     obstacles.push_back(make_pair(-26020, -15700));
-     obstacles.push_back(make_pair(-26020, -15680));
-     obstacles.push_back(make_pair(-26000, -18680));
-     obstacles.push_back(make_pair(-26000, -18660));
-     obstacles.push_back(make_pair(-26000, -18160));
-     obstacles.push_back(make_pair(-26000, -11120));
-     obstacles.push_back(make_pair(-25980, -18680));
-     obstacles.push_back(make_pair(-25980, -18480));
-     obstacles.push_back(make_pair(-25980, -18440));
-     obstacles.push_back(make_pair(-25980, -18160));
-     obstacles.push_back(make_pair(-25980, -18120));
-     obstacles.push_back(make_pair(-25980, -18100));
-     obstacles.push_back(make_pair(-25980, -15720));
-     obstacles.push_back(make_pair(-25980, -15680));
-     obstacles.push_back(make_pair(-25980, -13620));
-     obstacles.push_back(make_pair(-25980, -10100));
-     obstacles.push_back(make_pair(-25980, -10060));
-     obstacles.push_back(make_pair(-25980, -10040));
-     obstacles.push_back(make_pair(-25960, -18660));
-     obstacles.push_back(make_pair(-25960, -18580));
-     obstacles.push_back(make_pair(-25960, -18460));
-     obstacles.push_back(make_pair(-25960, -18440));
-     obstacles.push_back(make_pair(-25960, -18160));
-     obstacles.push_back(make_pair(-25960, -18120));
-     obstacles.push_back(make_pair(-25960, -13640));
-     obstacles.push_back(make_pair(-25960, -10020));
-     obstacles.push_back(make_pair(-25940, -18500));
-     obstacles.push_back(make_pair(-25940, -18140));
-     obstacles.push_back(make_pair(-25940, -15700));
-     obstacles.push_back(make_pair(-25940, -15680));
-     obstacles.push_back(make_pair(-25940, -13600));
-     // will include more obstacles
-        // Add more obstacles here
-    //};
+    vector<pair<int, int>> obstacles = {
+        {-26100, -18640}, {-26100, -18540}, {-26100, -18520}, {-26100, -18440},
+        {-26100, -18400}, {-26100, -18360}, {-26100, -18340}, {-26100, -18320},
+        {-26100, -18300}, {-26100, -18280}, {-26100, -18140}, {-26100, -18120},
+        {-26100, -15700}, {-26080, -18580}, {-26080, -18400}, {-26080, -18160},
+        {-26080, -18140}, {-26080, -15680}, {-26080, -11960}, {-26060, -18560},
+        {-26060, -18140}, {-26060, -15680}, {-26040, -18680}, {-26040, -18560},
+        {-26040, -18200}, {-26040, -18160}, {-26040, -18140}, {-26020, -18600},
+        {-26020, -18580}, {-26020, -18160}, {-26020, -15700}, {-26020, -15680},
+        {-26000, -18680}, {-26000, -18660}, {-26000, -18160}, {-26000, -11120},
+        {-25980, -18680}, {-25980, -18480}, {-25980, -18440}, {-25980, -18160},
+        {-25980, -18120}, {-25980, -18100}, {-25980, -15720}, {-25980, -15680},
+        {-25980, -13620}, {-25980, -10100}, {-25980, -10060}, {-25980, -10040},
+        {-25960, -18660}, {-25960, -18580}, {-25960, -18460}, {-25960, -18440},
+        {-25960, -18160}, {-25960, -18120}, {-25960, -13640}, {-25960, -10020},
+        {-25940, -18500}, {-25940, -18140}, {-25940, -15700}, {-25940, -15680},
+        {-25940, -13600},
+        // Add obstacle coordinates here
+    };
 
-     vector<pair<int, int>> lineSegments = {
-    { -25582, -12574 }, { -25581, -13468 },
-    { -25490, -13950 }, { -25489, -14764 },
-    { -25473, -14920 }, { -25472, -16072 },
-    { -25472, -16072 }, { -25471, -17360 },
-    { -25452, -5705 }, { -25451, -6452 },
-    { -25387, -17360 }, { -25385, -19792 },
-    { -25385, -12062 }, { -25383, -14193 },
-    { -25374, -14193 }, { -25373, -14920 },
-    { -25374, -6452 }, { -25372, -7681 },
-    { -25365, -7681 }, { -25360, -11770 },
-    { -25201, -19804 }, { -23558, -19802 },
-    { -24844, -15684 }, { -23692, -15682 },
-    { -23692, -15837 }, { -24844, -15839 },
-    { -23558, -19831 }, { -24197, -19831 },
-    { -21802, -19826 }, { -16128, -19819 },
-    { -21757, -19938 }, { -21802, -19826 },
-    { -21488, -19815 }, { -21757, -19938 },
-    { -21487, -19980 }, { -21488, -19815 },
-    { -19609, -19838 }, { -20818, -19840 },
-    { -18297, -18630 }, { -18259, -18332 },
-    { -18282, -18330 }, { -18282, -17668 },
-    { -17299, -6850 }, { -17300, -5279 },
-    { -16649, -17210 }, { -16650, -16578 },
-    { -16301, -6849 }, { -17299, -6850 },
-    { -16277, -17675 }, { -16277, -17241 },
-    { -16255, -16578 }, { -16256, -16049 },
-    { -16120, -13555 }, { -16121, -12442 },
-    { -16012, -19932 }, { -16015, -17111 },
-    { -15996, -4933 }, { -25421, -4944 },
-    { -15989, -17241 }, { -16001, -6277 },
-    { -15854, -12120 }, { -15855, -11586 }
- };
-
+    // Define line segments
+    vector<pair<int, int>> lineSegments = {
+        {-25582, -12574}, {-25581, -13468}, {-25490, -13950}, {-25489, -14764},
+        {-25473, -14920}, {-25472, -16072}, {-25472, -16072}, {-25471, -17360},
+        {-25452, -5705}, {-25451, -6452}, {-25387, -17360}, {-25385, -19792},
+        {-25385, -12062}, {-25383, -14193}, {-25374, -14193}, {-25373, -14920},
+        {-25374, -6452}, {-25372, -7681}, {-25365, -7681}, {-25360, -11770},
+        {-25201, -19804}, {-23558, -19802}, {-24844, -15684}, {-23692, -15682},
+        {-23692, -15837}, {-24844, -15839}, {-23558, -19831}, {-24197, -19831},
+        {-21802, -19826}, {-16128, -19819}, {-21757, -19938}, {-21802, -19826},
+        {-21488, -19815}, {-21757, -19938}, {-21487, -19980}, {-21488, -19815},
+        {-19609, -19838}, {-20818, -19840}, {-18297, -18630}, {-18259, -18332},
+        {-18282, -18330}, {-18282, -17668}, {-17299, -6850}, {-17300, -5279},
+        {-16649, -17210}, {-16650, -16578}, {-16301, -6849}, {-17299, -6850},
+        {-16277, -17675}, {-16277, -17241}, {-16255, -16578}, {-16256, -16049},
+        {-16120, -13555}, {-16121, -12442}, {-16012, -19932}, {-16015, -17111},
+        {-15996, -4933}, {-25421, -4944}, {-15989, -17241}, {-16001, -6277},
+        {-15854, -12120}, {-15855, -11586}
+        // Add line segment coordinates here
+    };
 
     // Calculate grid size
     int gridSizeX = abs(maxPosX - minPosX) / 100 + 1;
@@ -242,12 +191,12 @@ int main() {
     cout << "Starting A* search..." << endl;
 
     double timeRemaining = performAStar((minPosX + abs(minPosX)) / 100, (minPosY + abs(minPosY)) / 100, 
-                                    (maxPosX + abs(minPosX)) / 100, (maxPosY + abs(minPosY)) / 100, 
-                                    averageSpeed, obstacles, lineSegments, gridSizeX, gridSizeY);
-
+                                        (maxPosX + abs(minPosX)) / 100, (maxPosY + abs(minPosY)) / 100, 
+                                        averageSpeed, obstacles, lineSegments, gridSizeX, gridSizeY);
 
     if (timeRemaining >= 0) {
         cout << "Estimated time remaining: " << timeRemaining << " seconds" << endl;
+        cout << "Estimated distance remaining: " << timeRemaining * averageSpeed / 1000 << " meters" << endl;
     } else {
         cout << "Path not found!" << endl;
     }
